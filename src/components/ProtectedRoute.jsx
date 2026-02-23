6bb77e3e2f278e035b23b83289cf702439899081
nandana <nandanamanoj2020@gmail.com>
2026-02-23 19:10:31 +0530

    imported all the necessary components

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,6 @@
+import {Routes,Route} from "react-router-dom";
+import Landing from "../pages/landing";
+import Dashboard from "../features/dashboard/Dashboard";
+import Challenges from "../features/challenges/Challenges";
+import Leaderboard from "../features/leaderboard/Leaderboard";
+import AICoach from "../features/aiCoach/AICoach"
