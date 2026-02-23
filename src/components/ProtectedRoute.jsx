6e43fa9b65a88c448408a9aeca95a05c0a2b6e78
nandana <nandanamanoj2020@gmail.com>
2026-02-23 19:32:43 +0530

    added all the routes and added some routes in protected route

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -4,3 +4,29 @@ import Dashboard from "../features/dashboard/Dashboard";
 import Challenges from "../features/challenges/Challenges";
 import Leaderboard from "../features/leaderboard/Leaderboard";
 import AICoach from "../features/aiCoach/AICoach"
+
+const AppRoutes = ()=> {
+    return (
+        <Routes>
+            <Route path="/" element={<Landing/>}/>
+
+            <Route
+                path="/Dashboard" 
+                element={
+                    <ProtectedRoute>
+                        <Dashboard/>
+                    </ProtectedRoute>
+                }
+            />
+
+            <Route
+                path="/Challenges" 
+                element={
+                    <ProtectedRoute>
+                        <Challenges/>
+                    </ProtectedRoute>
+                }
+            />
+        </Routes>
+    )
+}
